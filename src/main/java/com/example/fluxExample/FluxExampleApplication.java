0bb83ffd1002bf26950a83aa207a651d1a903fc0
Patrick Sweeney <psweeney@nyc.rr.com>
2023-03-04 09:49:59 -0500

    initial commit

---
+++ src/main/java/com/example/fluxExample/FluxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fluxExample/FluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
